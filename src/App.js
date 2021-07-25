10b8f8d92c3a25ffe12f96071ad0664d0b1cbc6d
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2021-07-25 16:09:45 -0500

    redid the bonus section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react';
 import './App.css';
+import Loader from 'react-loader-spinner';
+ 
 
 function App() {
   const AboutUs = React.lazy(() => import('./AboutUs'))
@@ -11,7 +13,7 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-        <Suspense fallback={<h1>Loading...</h1>}>
+        <Suspense fallback={<Loader type="Bars" color="#FF00FF	" height={200} width={200} style={{margin: "0 auto"}}/>}>
           <AboutUs />
           <Packages />
         </Suspense>
