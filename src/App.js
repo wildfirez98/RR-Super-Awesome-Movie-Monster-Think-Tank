776c06fb4838cd02b0f05fd6d4a188977c09d412
wildfirez98 <james@areafamily.us>
2022-03-31 20:29:48 -0500

    First commit. Suspense implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<h1>Loading...</h1>}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
